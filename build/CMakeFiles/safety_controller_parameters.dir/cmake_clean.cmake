08ccaa74c086f3e0d3bab46d9da9d5eedd24159a
vivianamorlando <viviana.morlando@pal-robotics.com>
2025-01-14 10:30:37 +0100

    Cleaning pkg

--- build/CMakeFiles/safety_controller_parameters.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "safety_controller_parameters/include/safety_controller_parameters.hpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/safety_controller_parameters.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
